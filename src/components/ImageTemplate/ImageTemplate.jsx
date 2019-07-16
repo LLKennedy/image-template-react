4022d0667ac5df17996bcfd229a07beb06826486
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-07-16 22:55:16 +1000

    Add image-template dependency

---
+++ src/components/ImageTemplate/ImageTemplate.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import imgtmpl from 'image-template';
+
+export default class ImageTemplate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+
+    render() {
+        return (
+            <imgtmpl>
+            </imgtmpl>
+        )
+    }
+}
