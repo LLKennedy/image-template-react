0ee79917758540ff8d61580cb58632099d1ea682
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-07-19 22:45:30 +1000

    Attempt to reconfigure to fix build errors

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = function (api) {
+    api.cache(true);
+  
+    const presets = [ "@babel/preset-env" ];
+    const plugins = [ 
+        "@babel/plugin-proposal-object-rest-spread",
+        "@babel/plugin-transform-react-jsx" 
+    ];
+  
+    return {
+      presets,
+      plugins
+    };
+  }
